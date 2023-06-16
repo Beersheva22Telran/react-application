6af5e625c62a1980271879cfefbdd5300efdce21
Yuragranovsky <yuragranovsky@gmail.com>
2023-06-16 19:53:32 +0300

    clock with time zones

--- src/components/Clocks.tsx
+++ src/components/Clocks.tsx
@@ -2,18 +2,18 @@ import { useState, useEffect } from "react";
 import { Clock } from "./Clock"
 
 const Clocks:React.FC = () => {
-    const [time, setTime] = useState<string>(new Date().toLocaleTimeString())
+    const [time, setTime] = useState<Date>(new Date())
     useEffect(() => {
         const intervalId = setInterval(() => {
-       setTime(new Date().toLocaleTimeString(undefined,{timeZone:"Australia/Brisbane"}));
+       setTime(new Date());
       
    }, 1000 );
    return () => clearInterval(intervalId)
    }, [])
     return <div style={{display: 'flex',
      flexDirection: 'row', justifyContent: 'space-around'}}>
-       <Clock time={time} />
-       <Clock time={time}/>
+       <Clock time={time} cityCountry="London" />
+       <Clock time={time} cityCountry="Toronto"/>
     </div>
 }
 export default Clocks
