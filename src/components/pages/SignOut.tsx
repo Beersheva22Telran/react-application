26dffcb5517ecbc4b70c055e12b916b4951e9456
Yuragranovsky <yuragranovsky@gmail.com>
2023-07-09 11:02:45 +0300

    SignOut Button from MUI

--- src/components/pages/SignOut.tsx
+++ src/components/pages/SignOut.tsx
@@ -1,8 +1,10 @@
 import {useDispatch} from 'react-redux';
 import { authActions } from '../../redux/slices/authSlice';
+import { Button } from '@mui/material';
 const SignOut: React.FC = () => {
     const dispatch = useDispatch();
-    return <button onClick={() => dispatch(authActions.reset())}>confirm sign out</button>
+    return <
+        Button onClick={() => dispatch(authActions.reset())}>confirm sign out</Button>
 }
  
  export default SignOut;
